7ac9743129ba2ed61b9489606cdc4f61e994dd9b
pellurinavya <navya.p@codilar.com>
2023-05-04 17:52:41 +0530

    changes added

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -12,50 +12,52 @@ function NavBar() {
   return (
     <div>
       <div className={click ? "main-container" : ""} onClick={() => Close()} />
-      <nav className="navbar" onClick={(e) => e.stopPropagation()}>
-        <div className="nav-container">
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <a
-                href="/Welcome"
-                className="nav-links"
-                onClick={click ? handleClick : null}
-              >
-                Welcome to codilar
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="Sign"
-                className="nav-links"
-                onClick={click ? handleClick : null}
-              >
-                Sign In or{" "}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="/Create"
-                className="nav-links"
-                onClick={click ? handleClick : null}
-              >
-                Create an Account
-              </a>
-            </li>
-            <li className="nav-item">
-              <select
-                className="select-option"
-                onClick={click ? handleClick : null}
-              >
-                <option className="nav-links">vijayawadda</option>
-              </select>
-            </li>
-          </ul>
-          <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+      <div className="navyaNav">
+        <nav className="navbar" onClick={(e) => e.stopPropagation()}>
+          <div className="nav-container">
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <a
+                  href="/Welcome"
+                  className="nav-links"
+                  onClick={click ? handleClick : null}
+                >
+                  Welcome to codilar
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="Sign"
+                  className="nav-links"
+                  onClick={click ? handleClick : null}
+                >
+                  Sign In or{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="/Create"
+                  className="nav-links"
+                  onClick={click ? handleClick : null}
+                >
+                  Create an Account
+                </a>
+              </li>
+              <li className="nav-item">
+                <select
+                  className="select-option"
+                  onClick={click ? handleClick : null}
+                >
+                  <option className="nav-links">vijayawadda</option>
+                </select>
+              </li>
+            </ul>
+            <div className="nav-icon" onClick={handleClick}>
+              <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
       <div className="header-content">
         <div className={click ? "logo-img" : "logo "}>
           <img src={Starbucks} alt="" className="starLogo" />
